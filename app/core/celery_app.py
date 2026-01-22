a8c950f872031b512975f53590552014bc0af06d
Amayes <amayesh369@gmail.com>
2026-01-22 20:31:40 +0100

    Bug fixes

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -4,4 +4,4 @@ from app.core.config import settings
 
 celery_worker = Celery("school_worker", broker=settings.redis_worker_url, backend=settings.redis_worker_url)
 
-celery_worker.conf.task_routes = {"aoo.worker.tasks.*": {"queue": "main-queue"}}
+celery_worker.conf.task_routes = {"app.worker.tasks.*": {"queue": "main-queue"}}
