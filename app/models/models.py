f264f64b0be6fb7337fd03a44457d9708f08980f
Amayes <amayesh369@gmail.com>
2026-01-23 14:50:58 +0100

    Final update to the database tables

--- app/models/models.py
+++ app/models/models.py
@@ -10,8 +10,9 @@ class Student(Base):
     name = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String, index=True)
+    school_year = Column(Integer, index=True)
     token_version = Column(Integer, index=True)
-    grades = relationship("grade", back_populates="student")
+    grades = relationship("Grade", back_populates="student")
 
 class Teacher(Base):
     __tablename__ = "teacher"
@@ -20,26 +21,27 @@ class Teacher(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String, index=True)
     token_version = Column(Integer, index=True)
-    subjects = relationship("subject", back_populates="teacher")
+    subjects = relationship("Subject", back_populates="teacher")
 
 
 class Subject(Base):
     __tablename__ = "subject"
     id = Column(Integer, primary_key=True, index=True)
     subject_name = Column(String, index=True)
-    teacher_id = Column(Integer, ForeignKey("teacher.id", ondelete="CASCADE"))
-    teacher = relationship("teacher", back_populates="subjects")
+    teacher_id = Column(Integer, ForeignKey("teacher.id", ondelete="SET NULL"))
+    teacher = relationship("Teacher", back_populates="subjects")
+    grades = relationship("Grade", back_populates="subject")
 
 
 class Grade(Base):
     __tablename__ = "grade"
     id = Column(Integer, primary_key=True, index=True)
-    subject_name = Column(String, index=True)
     value = Column(Float)
     number = Column(Integer)
     subject_id = Column(Integer, ForeignKey("subject.id", ondelete="CASCADE"))
     student_id = Column(Integer, ForeignKey("student.id", ondelete="CASCADE"))
-    student = relationship("student", back_populates="grades")
+    subject = relationship("Subject", back_populates="grades")
+    student = relationship("Student", back_populates="grades")
 
 
 class Admin(Base):
